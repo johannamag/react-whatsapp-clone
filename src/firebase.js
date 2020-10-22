01acddc626d5a48a2c8768ccec074ce10663b825
johannamag <69107842+johannamag@users.noreply.github.com>
2020-10-22 16:21:29 +0200

    Project SetUp

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCZeX8JjURUNp-DLUqAnRG7mlk3G5WHT6A",
+  authDomain: "react-whatsappclone-johannamag.firebaseapp.com",
+  databaseURL: "https://react-whatsappclone-johannamag.firebaseio.com",
+  projectId: "react-whatsappclone-johannamag",
+  storageBucket: "react-whatsappclone-johannamag.appspot.com",
+  messagingSenderId: "31922110351",
+  appId: "1:31922110351:web:d5dc9edcc8a0d6858dd5fa",
+};
