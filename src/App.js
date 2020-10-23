cd40ff191bf8dbde64c7aca37f21fd4716227136
johannamag <69107842+johannamag@users.noreply.github.com>
2020-10-23 22:03:13 +0200

    Styled App and App Body

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="app__body"></div>
+    </div>
+  );
 }
 
 export default App;
