c293e49bd01178fea1b1796ad93aae08b5b044cd
johannamag <69107842+johannamag@users.noreply.github.com>
2020-10-25 11:10:06 +0100

    Installed Firebase + SetUp

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCZeX8JjURUNp-DLUqAnRG7mlk3G5WHT6A",
   authDomain: "react-whatsappclone-johannamag.firebaseapp.com",
@@ -7,3 +9,11 @@ const firebaseConfig = {
   messagingSenderId: "31922110351",
   appId: "1:31922110351:web:d5dc9edcc8a0d6858dd5fa",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
