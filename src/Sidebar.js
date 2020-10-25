0690ffd33c3b684521f80e5349049b9f058e350c
johannamag <69107842+johannamag@users.noreply.github.com>
2020-10-25 14:59:47 +0100

    Changes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Sidebar.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import ChatIcon from "@material-ui/icons/Chat";
